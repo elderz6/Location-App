e61d96d94f7bcbd8365313bedd3b74b4f99af7d4
elder oliveira <elder.oliveira@globalhitss.com.br>
2018-12-18 14:26:55 -0200

    room adds new users, return them correctly to app

--- index.js
+++ index.js
@@ -21,33 +21,45 @@ app.post('/api/user', async (req, res) =>{
       latitude: req.body.latitude,
       longitude: req.body.longitude
     }});
-    await Location.find({_id:'5c17b289bb5bb42968173b5b'}, async (err, sala) => {
+    await Location.find({_id:req.body.id}, async (err, sala) => {
       let checkUser = false;
       if (lodash.isEmpty(sala)) {
+        //if there is no entry matching the id, create a new room
         newItem.save();
+        res.status(200).send(newItem._id);
         console.log("saved new entry");
       }else {
         for (var i = 0; i < sala[0].users.length; i++) {
           if (sala[0].users[i].name == newItem.users[0].name) {
             console.log('user already exists');
+            //update user info if already exists
+            sala[0].users[i].latitude = newItem.users[0].latitude;
+            sala[0].users[i].longitude = newItem.users[0].longitude;
+            sala[0].save();
             checkUser = true;
           }
         }if (checkUser == false) {
+          //if the user is not on the list, push him to the array
             sala[0].users.push(newItem.users[0]);
             sala[0].save();
             console.log('new user pushed');
         }}
     });
-    res.status(200).send('Response ok--------------------------------------------------');
+});
+app.post('/api/user/room/search', async (req, res) => {
+  const local = await Location.find({'_id':req.body[0]._id})
+  .catch((err) => {
+    console.log(err);
+    res.status(200).send('id not found');
+  });
+  res.status(200).send(local[0].users);
 });
 
-app.get('/api/user', async (req, res) => {
-  const count = await Location.estimatedDocumentCount({});
-  const local = await Location.find();
+app.get('/api/user/:id', async (req, res) => {
+  const local = await Location.find({'_id':req.params.id});
   const users = [];
-  console.log(local);
-  for (var i = 0; i < count; i++) {
-    users.push(local[i].users)
+  for (var i = 0; i < local[0].users.length; i++) {
+    users.push(local[0].users[i])
   }
   res.status(200).send(users);
 });
